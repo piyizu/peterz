e33bb034a7965975b1e06e6b43d3b2aa72ff63c3
mrblug <1729403632@qq.com>
2022-06-26 16:45:51 +0800

    Create Moving data: indexed memory locations.s

---
+++ Assembly Language Learning/Moving data: indexed memory locations.s
@@ -0,0 +1,28 @@
+# compile this simply using gcc
+# gcc ./FILENAME.s -m32 -o ./OUT_FILENAME
+
+.section .data
+output:
+    .asciz "The value is %d\n"
+values:
+    .int 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
+
+.section .text
+.globl main
+main:
+    movl $0, %edi
+loop:
+    movl values(, %edi, 4), %eax
+    pushl %eax
+    pushl $output # '$' before a label means the address of the label
+    # pushed from the last argument to the first in printf()
+    call printf
+    addl $8, %esp # pushing will decrease %esp value
+    inc %edi
+    cmpl $11, %edi
+    jne loop
+    
+    movl $0, %ebx
+    movl $1, %eax
+    int $0x80
+
